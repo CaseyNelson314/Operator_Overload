2bcf044bbd1906e207e3fdb9d584de9d44cddb6f
CaseyNelson314 <caseynelson314@gmail.com>
2022-06-28 22:31:46 +0900

    add main file

---
+++ Operator_Overload.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "Int.h"
+
+int main()
+{
+    Int integer = 10;
+    integer += 100;
+    integer -= 100;
+    integer *= 100;
+    integer /= 100;
+
+    std::cout << l << " " << integer << std::endl;
+}
